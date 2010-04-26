e57149f1f1d5480e761ec8df07093fcec9b76dfa
youknowjack@gmail.com <youknowjack@gmail.com@544c33a2-2f3d-11df-9ac8-ff20597b2c9b>
2010-04-26 17:59:10 +0000

    changed to a table layout to fix display glitch

--- trunk/Input/templates/compact.php
+++ trunk/Input/templates/compact.php
@@ -1,10 +1,14 @@
 <div class="formitem">
-	<div class="labelspace">
-		<label for="%fieldname%">%label% <a href="javascript:;" onclick="javascript:toggleVisibility('%fieldname%_help');">[?]</a></label>
-	</div>
-	<div class="inputspace">
-		%code%
-	</div>
+	<table border="0">
+	<tr>
+		<td width="300px" valign="top">	
+			<label for="%fieldname%">%label% <a href="javascript:;" onclick="javascript:toggleVisibility('%fieldname%_help');">[?]</a></label>
+		</td>
+		<td>	
+			%code%
+		</td>
+	</tr>
+	</table>
 	<div class="help" style="visibility: hidden; display: none;" id="%fieldname%_help">
 		%shorthelp%
 	</div>
