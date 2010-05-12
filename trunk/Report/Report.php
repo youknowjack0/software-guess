6837ed73baf7320483abc0b393df43a010b72c15
youknowjack@gmail.com <youknowjack@gmail.com@544c33a2-2f3d-11df-9ac8-ff20597b2c9b>
2010-05-12 13:34:59 +0000

    reports

---
+++ trunk/Report/Report.php
@@ -0,0 +1,34 @@
+<?php
+class Report {
+    
+    var $fields = array();
+    
+    public static function newReport($type) {
+        if($type == 1) {
+            return new DetailedReport();
+        }
+    }
+    
+    function addField($key, $value) {
+        $fields[$key] = $value;    
+    }       
+    
+    function printReport($templatestring) { //children should override & call, should not be called directly
+        foreach($fields as $k => $v) {
+            $templatestring = str_replace("%%$k%%", $v, $templatestring);
+        }
+    }
+}
+
+class DetailedReport extends Report {
+    
+    static $template_file = 'templates/report1.html';
+    
+    function printReport() {       
+        ob_start();
+        require $template_file;
+        $template_code = ob_get_clean();
+        parent::printReport($template_code);
+    }    
+    
+}
